30b4b7872bb0ac77e94782ca7567c5a12a44de49
rajmohan-14 <rajmohan14k@gmail.com>
2026-02-14 10:19:33 +0530

    added delete and toggle feature

--- config/todo/urls.py
+++ config/todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todo_list),
+    path('', views.todo_list, name="home"),
+    path("delete/<int:id>/", views.delete_todo, name="delete_todo"),
+    path("toggle/<int:id>/", views.toggle_todo, name="toggle_todo"),
 ]
--- config/todo/views.py
+++ config/todo/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todo
+from django.shortcuts import redirect
 
 def todo_list(request):
     if request.method == "POST":
         title = request.POST.get("title")
         Todo.objects.create(title=title)
 
-        return redirect("/todo/")
+        return redirect("home")
 
     todos = Todo.objects.all()
     return render(request, "todo/list.html", {"todos": todos})
-# Create your views here.
+   
 
-
-# comment by keshu
+def delete_todo(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect("home")
+def toggle_todo(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.completed = not todo.completed
+    todo.save()
+    return redirect("home")
